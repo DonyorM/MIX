0c5ad5f12b9f5acf6e663eec87d7bf44777e4c1c
montyevans <montyevans@gmail.com>
2022-04-11 14:52:23 -0400

    Added Monty's diffusion method and fixed the requirements file

--- src/mix.py
+++ src/mix.py
@@ -2,9 +2,13 @@ from random import sample
 import scipy.sparse as sparse
 import networkx as nx
 import pandas as pd
+import numpy as np
 
-def MIX(adata, diffusion_level=2, input_obs='sample_labels', output_obs='mix_labels'):
-    conns = adata.obsp['connectivities']
+
+def MIX_daniel(
+    adata, diffusion_level=2, input_obs="sample_labels", output_obs="mix_labels_daniel"
+):
+    conns = adata.obsp["connectivities"]
     input_labels = adata.obs[input_obs]
     mix_labels = pd.Series(0, input_labels.index, name=input_labels.name)
     G = nx.from_scipy_sparse_array(conns)
@@ -15,11 +19,32 @@ def MIX(adata, diffusion_level=2, input_obs='sample_labels', output_obs='mix_lab
             if current_level < diffusion_level:
                 diffuse(to_node, current_level + 1, value)
 
-    i = 0
     for node in G.nodes():
         value = input_labels.iloc[node]
-        mix_labels.iloc[node] += value/(len(G[node]) + 1)
+        mix_labels.iloc[node] += value / (len(G[node]) + 1)
         diffuse(node, 1, value)
-        i += 1
-    
+
+    adata.obs[output_obs] = mix_labels
+
+
+def MIX_monty(
+    adata, num_steps, alpha=0.2, input_obs="sample_labels", output_obs="mix_labels"
+):
+    mix_labels = adata.obs[input_obs].astype(np.float32)
+    G = nx.from_scipy_sparse_array(adata.obsp["connectivities"])
+
+    steps_remaining = num_steps
+    while steps_remaining > 0:
+        if steps_remaining > len(G.nodes):
+            this_steps = len(G.nodes)
+        else:
+            this_steps = steps_remaining
+
+        steps_remaining -= this_steps
+
+        for seed_node in sample(G.nodes, this_steps):
+            for neighbour in G[seed_node]:
+                flow = (mix_labels[seed_node] - mix_labels[neighbour]) * alpha
+                mix_labels[neighbour] += flow
+
     adata.obs[output_obs] = mix_labels
